2e98f46ea09ef1580767c378fbed5ebcdd75c38b
JosehGaks <josehwilddog@gmail.com>
2020-02-07 17:07:05 +0300

    create a new sql connection

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,7 +6,6 @@ import org.sql2o.Sql2o;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
